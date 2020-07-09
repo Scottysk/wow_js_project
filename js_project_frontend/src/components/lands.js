45898bcf4ceed4070a70b1796ac12391725956b9
Scottysk <kwasnickisw@mail.irsc.edu>
2020-07-09 18:46:20 -0400

    added possibly(?) temporary seed files

--- js_project_frontend/src/components/lands.js
+++ js_project_frontend/src/components/lands.js
@@ -1,1 +1,17 @@
-.
+class Lands {
+	constructor() {
+		this.lands = []
+		this.adapter = new LandsAdapter()
+		this.bindEventListeners()
+		this.fetchAndLoadLands()
+	}
+
+
+	fetchAndLoadLands() {
+		this.adapter.getLands().then(lands => {
+			return console.log(lands)
+		})
+	}
+
+
+}
