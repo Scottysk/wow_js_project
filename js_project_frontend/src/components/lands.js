d27850994439a37cae1302f82535dfb67718a528
Scottysk <kwasnickisw@mail.irsc.edu>
2020-07-09 20:12:56 -0400

    fixed script order

--- js_project_frontend/src/components/lands.js
+++ js_project_frontend/src/components/lands.js
@@ -2,15 +2,23 @@ class Lands {
 	constructor() {
 		this.lands = []
 		this.adapter = new LandsAdapter()
-		this.bindEventListeners()
+		// this.bindEventListeners()
 		this.fetchAndLoadLands()
 	}
 
 
 	fetchAndLoadLands() {
 		this.adapter.getLands().then(lands => {
-			return console.log(lands)
+			lands.forEach(land => this.lands.push(land))
 		})
+		.then(() =>  {
+			this.render()
+		})
+	}
+
+	render() {
+		const landsContainer = document.getElementById('lands-container')
+		console.log("World of Warcraft's lands:", this.lands)
 	}
 
 
