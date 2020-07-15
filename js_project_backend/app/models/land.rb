556814aa92a43eab9d8ddc94aa15d4c88a1dbc0d
Scottysk <kwasnickisw@mail.irsc.edu>
2020-07-14 21:18:56 -0400

    restructured some code, added dropdown functionality for future zone adding use

--- js_project_backend/app/models/land.rb
+++ js_project_backend/app/models/land.rb
@@ -1,4 +1,4 @@
 class Land < ApplicationRecord
 	validates :name, presence: true
-	has_many :zone
+	has_many :zones
 end
--- js_project_frontend/src/land.js
+++ js_project_frontend/src/land.js
@@ -1,23 +1,27 @@
 
-//	renderLiNameAndDescription() {
-//		return `<li>${this.name}</li> ${this.description}`
-// }
 
 document.addEventListener("DOMContentLoaded", function() {
 	Land.createLand()
 	Land.getAllLands()
 })
 
+let dropDown = document.querySelector(".lands-select")
+let land = document.querySelector(".land")
+
+
+
+
 class Land {
 	constructor(landJSON) {
 		this.id = landJSON.id;
 		this.name = landJSON.name;
 		this.description = landJSON.description
+		this.zones = landJSON.zones
 
 	}
  
 	static createLand() {
-		let landForm = document.querySelector(".favorite-land-form")
+		let landForm = document.querySelector(".new-land-form")
 		landForm.addEventListener("submit", function(event) {
 			event.preventDefault()
 
@@ -51,6 +55,8 @@ class Land {
 
 
 			let newLand = new Land(c)
+			dropDown.innerHTML = dropDown.innerHTML + `<option value="${c.id}">${c.name}</option>`
+
 			newLand.addLandtoDom()
 			})
 		})
@@ -62,6 +68,14 @@ class Land {
 		let p = document.createElement("p")
 		p.innerText = this.name
 		land.appendChild(p)
+	}
+
+	static getDropDownChange() {
+
+		dropDown.addEventListener("change", function(event) {
+			land.innerHTML = "";
+			console.log("event", event.target.value)
+		})
 	}
 
 
